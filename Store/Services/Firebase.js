2f8371a99d4a4f9305807770dcc8fa8aa5da8d96
Daniel Montoya <dmontoya.web@gmail.com>
2018-11-05 12:53:55 -0500

    Clase 40

---
+++ Store/Services/Firebase.js
@@ -0,0 +1,11 @@
+import * as firebase from 'firebase';
+
+const config = {
+  apiKey: 'AIzaSyDyx9SPW_clp7gm7g6CX38T6ylZIt9RNIE',
+  authDomain: 'intagram-12345.firebaseapp.com',
+  databaseURL: 'https://intagram-12345.firebaseio.com',
+  projectId: 'intagram-12345',
+  storageBucket: 'intagram-12345.appspot.com',
+  messagingSenderId: '610032864689',
+};
+firebase.initializeApp(config);
