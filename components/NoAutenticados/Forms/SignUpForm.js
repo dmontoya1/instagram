828fc046c9529b3bd85b99a8fe6ea6173ad4d6c7
Daniel Montoya <dmontoya.web@gmail.com>
2018-11-05 11:31:17 -0500

    Clase 36

--- components/NoAutenticados/Forms/SignUpForm.js
+++ components/NoAutenticados/Forms/SignUpForm.js
@@ -4,20 +4,27 @@ import {
 } from 'react-native';
 import { Field, reduxForm } from 'redux-form';
 
-const fieldName = props => (
-  //   console.log(props);
-  <TextInput
-    placeholder="Texto desde field nombre"
-    onChangeText={props.input.onChange}
-    value={props.input.value}
-  />
-);
+const fieldName = (props) => {
+  console.log(props);
+  return (
+    <TextInput
+      placeholder={props.ph}
+      onChangeText={props.input.onChange}
+      value={props.input.value}
+      keyboardType={props.input.name === 'email' ? 'email-address' : 'default'}
+      autoCapitalize="none"
+      secureTextEntry={!!(props.input.name === 'password' || props.input.name === 'password2')}
+    />
+  );
+};
 const SignUpForm = (props) => {
   console.log(props);
   return (
     <View>
-      <Field name="first_name" component={fieldName} />
-      <Field name="email" component={fieldName} />
+      <Field name="name" component={fieldName} ph="Nombre" />
+      <Field name="email" component={fieldName} ph="correo@correo.com" />
+      <Field name="password" component={fieldName} ph="******" />
+      <Field name="password2" component={fieldName} ph="******" />
       <Text>Redux Form</Text>
       <Button
         title="Registrar"
